28f87417060a36f1fdc09763eae48abca675d07b
MiguelC29 <miguelito28c@gmail.com>
2024-06-11 18:52:27 -0500

    agregando nuevo proyecto .net core, con CRUD Jardin

---
+++ ICBFApp/Pages/Jardin/Create.cshtml.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using static ICBFApp.Pages.Jardin.IndexModel;
+using System.Data.SqlClient;
+
+namespace ICBFApp.Pages.Jardin
+{
+    public class CreateModel : PageModel
+    {
+        public JardinInfo jardinInfo = new JardinInfo();
+        public string errorMessage = "";
+        public string successMessage = "";
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            jardinInfo.nombre = Request.Form["nombreJardin"];
+            jardinInfo.direccion = Request.Form["direccionJardin"];
+            jardinInfo.estado = Request.Form["estado"];
+
+            if (jardinInfo.nombre.Length == 0 || jardinInfo.direccion.Length == 0 || jardinInfo.estado.Length == 0)
+            {
+                errorMessage = "Debe completar todos los campos";
+                return;
+            }
+
+            try
+            {
+                String connectionString = "Data Source=PC-MIGUEL-C\\SQLEXPRESS;Initial Catalog=db_ICBF;Integrated Security=True;";
+                //String connectionString = "RUTA ANGEL";
+                //String connectionString = "RUTA SENA";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    String sqlExists = "SELECT COUNT(*) FROM jardines WHERE nombre = @nombreJardin";
+                    using (SqlCommand commandCheck = new SqlCommand(sqlExists, connection))
+                    {
+                        commandCheck.Parameters.AddWithValue("@nombreJardin", jardinInfo.nombre);
+
+                        int count = (int)commandCheck.ExecuteScalar();
+
+                        if (count > 0)
+                        {
+                            errorMessage = "El Jardín '" + jardinInfo.nombre + "' ya existe. Verifique la información e intente de nuevo.";
+                            return;
+                        }
+                    }
+
+                    // Espacio para validar que el jadin no exista
+                    String sqlInsert = "INSERT INTO jardines (nombre, direccion, estado)" +
+                        "VALUES (@nombreJardin, @direccionJardin, @estado);";
+
+                    using (SqlCommand command = new SqlCommand(sqlInsert, connection))
+                    {
+                        command.Parameters.AddWithValue("@nombreJardin", jardinInfo.nombre);
+                        command.Parameters.AddWithValue("@direccionJardin", jardinInfo.direccion);
+                        command.Parameters.AddWithValue("@estado", jardinInfo.estado);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+
+            jardinInfo.nombre = "";
+            jardinInfo.direccion = "";
+            jardinInfo.estado = "";
+
+            successMessage = "Jardín agregado con éxito";
+            Response.Redirect("/Jardin/Index");
+        }
+    }
+}

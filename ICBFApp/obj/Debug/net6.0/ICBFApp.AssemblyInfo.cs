8b5d3df7d3d848628ccce03fb4e8c0e6523a333f
MiguelC29 <miguelito28c@gmail.com>
2024-06-21 23:57:01 -0500

    diseño index jardines colores estados

--- ICBFApp/obj/Debug/net6.0/ICBFApp.AssemblyInfo.cs
+++ ICBFApp/obj/Debug/net6.0/ICBFApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICBFApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b2249c1fd94a53327c298ff909b3882a143238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1ce503dd7840fdd61f652952626bfc06de0b5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICBFApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICBFApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
